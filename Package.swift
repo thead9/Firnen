19086ec88945093337fb50d6567788792afbbeb9
Thomas Headley <thomasheadleyjr@gmail.com>
2024-04-01 21:43:56 -0400

    ValidatedField

--- Package.swift
+++ Package.swift
@@ -4,20 +4,21 @@
 import PackageDescription
 
 let package = Package(
-    name: "Firnen",
-    products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "Firnen",
-            targets: ["Firnen"]),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "Firnen"),
-        .testTarget(
-            name: "FirnenTests",
-            dependencies: ["Firnen"]),
-    ]
+  name: "Firnen",
+  platforms: [.iOS(.v17)],
+  products: [
+    // Products define the executables and libraries a package produces, making them visible to other packages.
+    .library(
+      name: "Firnen",
+      targets: ["Firnen"]),
+  ],
+  targets: [
+    // Targets are the basic building blocks of a package, defining a module or a test suite.
+    // Targets can depend on other targets in this package and products from dependencies.
+    .target(
+      name: "Firnen"),
+    .testTarget(
+      name: "FirnenTests",
+      dependencies: ["Firnen"]),
+  ]
 )
---
+++ Sources/Firnen/ValidatedField.swift
@@ -0,0 +1,53 @@
+//
+//  ValidatedField.swift
+//  Firnen
+//
+//  Created by Thomas Headley on 3/26/24.
+//
+
+import SwiftUI
+
+public struct ValidatedField: ViewModifier {
+  private var isValid: Bool
+  private var invalidNotice: String
+  
+  public init(isValid: Bool, invalidNotice: String) {
+    self.isValid = isValid
+    self.invalidNotice = invalidNotice
+  }
+  
+  public init(isValid: Bool, invalidNotice: InvalidNotice) {
+    self.init(isValid: isValid, invalidNotice: invalidNotice.displayName)
+  }
+  
+  public func body(content: Content) -> some View {
+    VStack(alignment: .leading) {
+      content
+      if !isValid {
+        Text(invalidNotice)
+          .foregroundStyle(.red)
+      }
+    }
+  }
+  
+  public enum InvalidNotice {
+    case required
+    
+    var displayName: String {
+      switch self {
+      case .required:
+        "* Required"
+      }
+    }
+  }
+}
+
+public extension View {
+  func validatedField(isValid: Bool, invalidNotice: String) -> some View {
+    modifier(ValidatedField(isValid: isValid, invalidNotice: invalidNotice))
+  }
+  
+  func validatedField(isValid: Bool, invalidNotice: ValidatedField.InvalidNotice) -> some View {
+    modifier(ValidatedField(isValid: isValid, invalidNotice: invalidNotice))
+  }
+}
